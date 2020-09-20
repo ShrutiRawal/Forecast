10c0c2df941affddf4eb1b04ff60eaf63701371d
ShrutiRawal <shruti456rawal@gmail.com>
2020-09-20 23:35:36 +0530

    Retrofit initialised , weather response retrieved

---
+++ app/src/main/java/com/example/forecast/data/ApixuWeatherApiService.kt
@@ -0,0 +1,52 @@
+package com.example.forecast.data
+
+import com.example.forecast.data.response.CurrentWeatherResponse
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import kotlinx.coroutines.Deferred
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+const val API_KEY = "7e8bc3542e5d9b2ac48d8a1062f5dfcf"
+
+interface ApixuWeatherApiService {
+    @GET("current")
+    fun getCurrentWeather(
+        @Query("query") location: String
+    ): Deferred<CurrentWeatherResponse>
+
+    companion object {
+        operator fun invoke(): ApixuWeatherApiService {
+            val requestInterceptor = Interceptor { chain ->
+
+                val url = chain.request()
+                    .url()
+                    .newBuilder()
+                    .addQueryParameter("access_key", API_KEY)
+                    .build()
+                val request = chain.request()
+                    .newBuilder()
+                    .url(url)
+                    .build()
+
+                return@Interceptor chain.proceed(request)
+            }
+
+             val okHttpClient = OkHttpClient.Builder()
+                .addInterceptor(requestInterceptor)
+                //.addInterceptor(connectivityInterceptor)
+                .build()
+
+            return Retrofit.Builder()
+                .client(okHttpClient)
+                .baseUrl("http://api.weatherstack.com/")
+                .addCallAdapterFactory(CoroutineCallAdapterFactory())
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(ApixuWeatherApiService::class.java)
+        }
+        }
+    }
--- app/src/main/java/com/example/forecast/ui/weather/current/CurrentWeatherFragment.kt
+++ app/src/main/java/com/example/forecast/ui/weather/current/CurrentWeatherFragment.kt
@@ -8,6 +8,12 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.example.forecast.R
+import com.example.forecast.data.ApixuWeatherApiService
+import kotlinx.android.synthetic.main.current_weather_fragment.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class CurrentWeatherFragment : Fragment() {
 
@@ -28,6 +34,13 @@ class CurrentWeatherFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(CurrentWeatherViewModel::class.java)
         // TODO: Use the ViewModel
+
+        val apiService = ApixuWeatherApiService()
+        GlobalScope.launch(Dispatchers.Main){
+            val currentWeatherResponse = apiService.getCurrentWeather("Varanasi").await()
+            current.text = currentWeatherResponse.toString()
+        }
+
     }
 
 }
